da3281885845e8d51e1bdc4debd22ffbe80f8e72
카노링 <kanoring9304@gmail.com>
2020-01-17 15:12:02 +0900

    no message

---
+++ Material.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private MeshRenderer mesh;
+
+
+
+    [SerializeField] Material red_Mat;
+    [SerializeField] Material green_Mat;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mesh = GetComponent<MeshRenderer>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mesh.material = green_Mat;
+        }
+
+        
+
+        else
+        {
+            mesh.material = red_Mat;
+        }
+        
+    }
+}
